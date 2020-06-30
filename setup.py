a2ed9796f6d6d36e3761084fd5734b53e2dc912b
Doni Afrizal <doni.afrizal@gmail.com>
2020-06-30 16:55:05 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in shipment_management/__init__.py
@@ -13,6 +13,7 @@ with open('shipment_management/__init__.py', 'rb') as f:
 with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
+	
 setup(
 	name='shipment_management',
 	version=version,
@@ -22,5 +23,6 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires
+	install_requires=[str(ir.req) for ir in requirements],
+	dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
